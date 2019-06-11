744db0951ca1ef84ce5eefbfd1b9f1b251e6ff8b
marianamv112 <marianamv112@gmail.com>
2019-06-11 16:23:47 +0100

    LIS Mariana Done

--- js/navigation.js
+++ js/navigation.js
@@ -1,10 +1,27 @@
+let $links = document.querySelectorAll('a');
+let $routes = document.querySelectorAll('[data-route]');
+
 function listenForLinksClick() {
-  // TODO
-  let $links // = ...
+  $links.forEach( $link => {
+    $link.onclick = (event) => { //this is a parameter event 
+      event.preventDefault(); //Stop the default behaviour, that would redirect to another page
+      let href = $link.getAttribute('href');
+      console.log('You clicked on a link where href is ',href);
+      displayOneSectionAndHideOthers(href);
+    }
+  }) //foreach only works with querySelectorAll!!!
 }
 
 function displayOneSectionAndHideOthers(href) {
-  // TODO
+  $routes.forEach($route => {
+    let dataRoute = $route.getAttribute('data-route');
+    if (dataRoute === href) {
+      $route.style.display = 'block';
+    } else {
+      $route.style.display = 'none'
+    }
+  });
 }
 
 listenForLinksClick()
+displayOneSectionAndHideOthers('/')
